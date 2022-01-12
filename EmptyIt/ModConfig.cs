6b4aa973bf24013205198c4ec731d6411307fe68
Keallu <45124091+keallu@users.noreply.github.com>
2022-01-12 19:04:53 +0100

    Version 1.3.

--- EmptyIt/ModConfig.cs
+++ EmptyIt/ModConfig.cs
@@ -5,17 +5,17 @@
     {
         public bool ConfigUpdated { get; set; }
         public int Interval { get; set; } = 1;
-        public bool EmptyWarehouses { get; set; } = true;
+        public bool EmptyWarehouses { get; set; } = false;
         public int UpperThresholdWarehouses { get; set; } = 75;
-        public bool StopEmptyingWarehouses { get; set; } = true;
+        public bool StopEmptyingWarehouses { get; set; } = false;
         public int LowerThresholdWarehouses { get; set; } = 25;
         public bool EmptyLandfillSites { get; set; } = true;
         public int UpperThresholdLandfillSites { get; set; } = 75;
         public bool StopEmptyingLandfillSites { get; set; } = true;
         public int LowerThresholdLandfillSites { get; set; } = 25;
-        public bool EmptyWasteTransferFacilities { get; set; } = true;
+        public bool EmptyWasteTransferFacilities { get; set; } = false;
         public int UpperThresholdWasteTransferFacilities { get; set; } = 75;
-        public bool StopEmptyingWasteTransferFacilities { get; set; } = true;
+        public bool StopEmptyingWasteTransferFacilities { get; set; } = false;
         public int LowerThresholdWasteTransferFacilities { get; set; } = 25;
         public bool EmptyCemeteries { get; set; } = true;
         public int UpperThresholdCemeteries { get; set; } = 75;
